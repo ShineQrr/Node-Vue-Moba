ce98f21613448b92f1caf728834c113a0244a86a
shenjinqing <sjq_nuaa@163.com>
2020-07-22 15:45:55 +0800

    server添加编辑分类接口

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -3,10 +3,31 @@ module.exports = app => {
     const router = express.Router()
     const Category = require('../../models/Category')
 
+    // 新建分类
     router.post('/categories', async (req, res) => {
-        console.log(req.body);
         const model = await Category.create(req.body)
         res.send(model)
     })
+
+    // 编辑分类，需要在路径中携带id
+    router.put('/categories/:id', async (req, res) => {
+        const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+        res.send(model)
+    })
+
+    // 获取分类列表
+    router.get('/categories', async (req, res) => {
+        // 找到10条数据
+        const items = await Category.find().limit(10)
+        res.send(items)
+    })
+
+    // 查找某条数据
+    router.get('/categories/edit/:id', async (req, res) => {
+        // 根据id找到分类名称
+        const item = await Category.findById(req.params.id)
+        res.send(item)
+    })
+
     app.use('/admin/api', router)
 }
