4a0040e8203358e8b52542b303c3ad57becd733c
shenjinqing <sjq_nuaa@163.com>
2020-07-22 13:44:38 +0800

    admin对接新建分类接口

--- admin/src/plugins/element.js
+++ admin/src/plugins/element.js
@@ -1,4 +1,24 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, Container, Aside, Menu, Submenu, MenuItemGroup, Header, Dropdown, Table, TableColumn, DropdownItem, Main, MenuItem, DropdownMenu, Input, FormItem, Message } from 'element-ui'
+
+Vue.prototype.$message = Message
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+
+Vue.use(Input)
+
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(Header)
+Vue.use(Dropdown)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(DropdownItem)
+Vue.use(Main)
+Vue.use(MenuItem)
+Vue.use(DropdownMenu)
---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>创建分类</h1>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="名称">
+        <el-input v-model="model.name" placeholder="请输入名称"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      model: {}
+    };
+  },
+  methods: {
+    // 需要请求接口，提交数据
+    async save() {
+      const res = await this.$http.post("/categories", this.model);
+      console.log(res);
+      // 跳转到分类列表
+      this.$router.push("/categories/list");
+      this.$message({
+        message: "保存成功",
+        type: "success"
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
